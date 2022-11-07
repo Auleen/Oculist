538c8359964e0b60359616344363c30b2ddbe2a6
Pious <auleenghosh.sg@gmail.com>
2022-11-07 22:48:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import About from "./components/about/about";
+import Search from "./components/report/report";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Database from "./components/database/db";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <About />
+              </>
+            }
+          />
+          <Route path="/Report" element={<Search />} />
+          <Route path="/database" element={<Database />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './navbar.css'
+import {Link} from "react-router-dom"
+function Navbar() {
+    return ( 
+        <div className='Navbar'>
+            <div className='Logo'>
+                <h1>Oculist 🧿</h1>
+            </div>
+        <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/report">Report</Link></li>
+            <li><Link to="/database">Database</Link></li>
+        </ul>
+        </nav>
+        <button  onclick="location.href='https://github.com/Auleen/'">Star ✨</button>
+
+
+        </div>
+     );
+}
+
+export default Navbar;
